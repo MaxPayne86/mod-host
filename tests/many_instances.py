cc34719ce45652e7c7ce81b381f85b3e196c7463
Massimo Pennazio <maxipenna@libero.it>
2017-10-09 17:54:27 +0200

    Fixed various bugs in test script

--- tests/many_instances.py
+++ tests/many_instances.py
@@ -5,21 +5,18 @@
 
 import socket, time
 import subprocess as sp
-from tornado import ioloop
 from psutil import Process
-import socket, functools, json
-import psutil
 
-def check_mod_host():
+def check_mod_host(pid):
     if sp.check_output("pgrep mod-host; exit 0", shell=True) != pid:
         print('mod-host died')
         exit(1)
 
-def send_command(command):
+def send_command(pid, s, command):
     print('sent:', command)
     s.send(str.encode(command))
     
-    check_mod_host()
+    check_mod_host(pid)
 
     try:
         resp = s.recv(1024)
@@ -41,11 +38,12 @@ def main():
     s.connect(('localhost', 5555))
     s.settimeout(0.5)
 
-    jackdpid = sp.check_output("pgrep jackd; exit 0", shell=True)
-    p = Process(jackdpid)
+    #jackdpid = sp.check_output("pgrep jackd; exit 0", shell=True)
+    #p = Process(int(jackdpid))
+    p = Process(int(pid))
 
     for i in range(25):
-        send_command('add http://lv2plug.in/plugins/eg-amp %i' % i)
-        print("CPU:{0} RAM:{1}".format(p.get_cpu_percent(),p.get_memory_percent()))
+        send_command(pid, s, 'add http://lv2plug.in/plugins/eg-amp %i' % i)
+        print("CPU:{0} RAM:{1}".format(p.cpu_percent(),p.memory_percent()))
                                           
 if __name__ == "__main__": main()
